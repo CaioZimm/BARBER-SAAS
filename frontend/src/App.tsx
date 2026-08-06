ed54744d0c3a24d1f0751e167a5c16b9d385ce36
Caio Venancio <caiovenancio06@gmail.com>
2026-08-06 17:00:07 -0300

    feat: adicionar favicon no projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './contexts/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { queryClient } from './lib/react-query'
+import { Toaster } from 'react-hot-toast'
 import { AppRoutes } from './routes'
 
 function App() {
@@ -10,6 +11,16 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <AppRoutes />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                background: '#18181b', // zinc-900
+                color: '#fff',
+                border: '1px solid #27272a' // zinc-800
+              }
+            }}
+          />
         </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
